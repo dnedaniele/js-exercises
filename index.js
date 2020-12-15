d012ab9cd9c5603a0d0f388365b4ae77e550187f
Daniele Esposito <esp.dne@gmail.com>
2020-12-15 19:23:51 +0100

    async await

---
+++ index.js
@@ -0,0 +1,14 @@
+function shootArrow (){
+    console.log("Hold...");
+    console.log("Shoot Arrows!")
+}
+    
+
+
+function giveCommand (command){
+setTimeout(command, 2000);
+}
+
+console.log("-------------");
+
+giveCommand(shootArrow); 
